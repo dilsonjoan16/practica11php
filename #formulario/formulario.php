9a06be9f7ceacbc84ccc941ed92f1c5e714f2bb3
Dilsonjoan16 <dilsonjoan16@gmail.com>
2020-08-19 13:30:05 -0400

    first commit

---
+++ #formulario/formulario.php
@@ -0,0 +1,50 @@
+<?php
+$nombre = $_POST["nombre"];
+$password = $_POST["password"];
+$genero = $_POST["genero"];
+$email = $_POST["email"];
+$materia = $_POST["materia"];
+$telefono = $_POST["telefono"];
+
+if (!empty($nombre)||!empty($password)||!empty($genero)||!empty($email)||!empty($materia)||!empty($telefono) ){
+    $host = "localhost";
+    $dbusername="root";
+    $dbpassword="";
+    $dbname="estudiantes";
+
+    $conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
+    if (mysqli_connect_error()){
+        die('connect error('.mysqli_connect_errno().')'.mysqli_connect_error());
+    }
+    else {
+        $SELECT="SELECT telefono from usuario where telefono = ? limit 1";
+        $INSERT="INSERT INTO usuario(nombre,password,genero,email,materia,telefono)values(?,?,?,?,?,?)";
+
+        $stmt=$conn-> prepare($SELECT);
+        $stmt ->bind_param("i",$telefono);
+        $stmt ->execute();
+        $stmt ->bind_result($telefono);
+        $stmt ->store_result();
+        $rnum =$stmt->num_rows();
+        if ($rnum==0) {
+            $stmt->close();
+            $stmt=$conn-> prepare($INSERT);
+            $stmt->bind_param("sssssi",$nombre,$password,$genero,$email,$materia,$telefono);
+            $stmt ->execute();
+            echo "REGISTRO COMPLETADO.";
+        }
+        else {
+            echo"Alguien ya registro ese numero de telefono.<br>";
+            echo"Le sugerimos intentar con uno nuevo.<br>";
+        }
+        $stmt->close();
+        $conn->close();
+    }
+}
+else {
+    echo "Todos los datos son OBLIGATORIOS";
+    die();
+}
+
+
+?>
